0415e7908d4b0b4a9dbdfab79eed949122623934
dammkewl <dammkewl>
2022-08-01 22:34:22 +0200

    code reorder and reference to electrs.toml is now an absolute reference

--- docker/my-dojo/electrs/restart.sh
+++ docker/my-dojo/electrs/restart.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+sed -i \
+  -e "s/BITCOIND_RPC_USER/${BITCOIND_RPC_USER}/g" \
+  -e "s/BITCOIND_RPC_PASSWORD/${BITCOIND_RPC_PASSWORD}/g" \
+  -e "s/DOJO_VERSION_TAG/${DOJO_VERSION_TAG}/g" \
+  /electrs.toml
+
 indexer_options=(
   --log-filters="INFO"
   --index-batch-size="$INDEXER_BATCH_SIZE"
@@ -9,12 +15,6 @@ indexer_options=(
   --daemon-p2p-addr="$BITCOIND_IP:8333"
   --index-lookup-limit
 )
-
-sed -i \
-  -e "s/BITCOIND_RPC_USER/${BITCOIND_RPC_USER}/g" \
-  -e "s/BITCOIND_RPC_PASSWORD/${BITCOIND_RPC_PASSWORD}/g" \
-  -e "s/DOJO_VERSION_TAG/${DOJO_VERSION_TAG}/g" \
-  ./electrs.toml
 
 if [ "$COMMON_BTC_NETWORK" == "testnet" ]; then
   indexer_options+=(--network="testnet")
